134667621df35bd639f5a4d7c65e96c25960f430
Ferdi Ananta Salman <ferdiananta51@gmail.com>
2023-10-01 16:42:19 +0700

    test commit

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -12,6 +12,7 @@
 
   <ul class="menu-inner py-1">
     <!-- Menu -->
+    <!-- test -->
     <li class="menu-header small text-uppercase"><span class="menu-header-text">Menu Utama</span></li>
 
     <!-- Dashboard -->
